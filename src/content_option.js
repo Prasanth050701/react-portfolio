329033bc764103e3c85aea8396d4c9546c2e6e17
Prasanth_05 <axechandru@gmail.com>
2023-08-19 23:43:28 +0530

    git commit -m "react project"

--- src/content_option.js
+++ src/content_option.js
@@ -1,11 +1,11 @@
-const logotext = "JOHN";
+const logotext = "PRAS";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Prashanth S",
+    description: "I'm Prashanth UI Full stack devloper,currently working in Madurai",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I'm Prashanth",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
@@ -37,11 +37,7 @@ const worktimeline = [{
 ];
 
 const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
+        name: "Angular",
         value: 85,
     },
     {
@@ -139,19 +135,17 @@ const contactConfig = {
     YOUR_EMAIL: "name@domain.com",
     YOUR_FONE: "(555)123-4567",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
     YOUR_USER_ID: "user_id",
 };
 
-const socialprofils = {
+  const socialprofils = {
     github: "https://github.com",
     facebook: "https://facebook.com",
     linkedin: "https://linkedin.com",
     twitter: "https://twitter.com",
-};
+}; 
 export {
     meta,
     dataabout,
